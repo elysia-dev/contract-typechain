9ac59ceedf105825c58078f6f0f58c8d768f247f
donguk.seo <donguk.seo@modori.io>
2021-08-10 18:57:42 +0900

    remove dist

--- dist/src/StakingPool/index.js
+++
@@ -1,8 +0,0 @@
-export { ERC20__factory } from "./factories/ERC20__factory";
-export { IERC20Metadata__factory } from "./factories/IERC20Metadata__factory";
-export { IERC20__factory } from "./factories/IERC20__factory";
-export { IStakingPool__factory } from "./factories/IStakingPool__factory";
-export { StakingPoolLogic__factory } from "./factories/StakingPoolLogic__factory";
-export { StakingPool__factory } from "./factories/StakingPool__factory";
-export { RewardAsset__factory } from "./factories/RewardAsset__factory";
-export { StakingAsset__factory } from "./factories/StakingAsset__factory";
